64fb590a6aac1cd2e66d32ae4bd9b188bf04aa82
hai <525300595@qq.com>
2019-02-18 13:44:50 +0800

    init

---
+++ Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Category
+//
+//  Created by hai8 on 2019/2/18.
+//  Copyright © 2019年 hai8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Category
+//
+//  Created by hai8 on 2019/2/18.
+//  Copyright © 2019年 hai8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
